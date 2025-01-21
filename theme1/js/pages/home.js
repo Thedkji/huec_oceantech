91be23e9e1211e4db4c57d2f6d542afa420aa009
Quang <nmquang2303@gmail.com>
2025-01-21 11:43:42 +0700

    reponsive lại trang home và sửa 1 số chi tiết

--- theme1/js/pages/home.js
+++ theme1/js/pages/home.js
@@ -11,23 +11,23 @@ function home() {
       '<button type="button" class="slick-prev"><img src="./theme1/images/icon/left-arrow-block-2.svg" style="width:40px;height:40px"</button>',
     nextArrow:
       '<button type="button" class="slick-next"><img src="./theme1/images/icon/right-arrow-block-2.svg" style="width:40px;height:40px"</button>',
-      responsive: [
-        {
-          breakpoint: 1025,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-          },
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
         },
-        {
-          breakpoint: 769,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
-          },
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
         },
-      ],
+      },
+    ],
   });
 
   $("#home-hot-title-section>div").slick({
@@ -56,7 +56,17 @@ function home() {
           slidesToShow: 2,
           slidesToScroll: 1,
           arrows: false,
-          autoplay:true,
+          autoplay: true,
+          autoplaySpeed: 3000,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          autoplay: true,
           autoplaySpeed: 3000,
         },
       },
@@ -75,25 +85,25 @@ function home() {
       '<button type="button" class="slick-prev"><i class="icon-chervon-left "></i></button>',
     nextArrow:
       '<button type="button" class="slick-next"><i class="icon-chervon-right "></i></button>',
-      responsive: [
-        {
-          breakpoint: 1025,
-          settings: {
-            slidesToShow: 5,
-            slidesToScroll: 1,
-          },
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
         },
-        {
-          breakpoint: 769,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            arrows: false,
-            autoplay:true,
-            autoplaySpeed: 3000,
-          },
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          arrows: false,
+          autoplay: true,
+          autoplaySpeed: 3000,
         },
-      ],
+      },
+    ],
   });
 }
 
